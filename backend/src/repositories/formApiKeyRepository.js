45a0b1f327366e6b95cda7a40b39dec03968e622
shad <23f3004148@ds.study.iitm.ac.in>
2026-01-15 20:12:08 +0530

    Delete backend directory

--- backend/src/repositories/formApiKeyRepository.js
+++
@@ -1,7 +0,0 @@
-const FormApiKey = require('../models/formApiKey');
-
-const createKey = (payload) => FormApiKey.create(payload);
-
-module.exports = {
-  createKey,
-};
--- backend/src/utils/env.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-// Always load backend/.env regardless of the current working directory
-const envPath = path.join(__dirname, '..', '..', '.env');
-require('dotenv').config({ path: envPath });
-
-module.exports = process.env;
